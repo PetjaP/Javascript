e09867b1e82c871e5050cf7e42e5e8f5bb49ee4f
Petja Penttinen <petja.penttinen@edu.keuda.fi>
2022-10-27 11:18:15 +0300

    Testaus

---
+++ HTML/testaus/aritmeettiset.js
@@ -0,0 +1,6 @@
+// Yksilotestit
+const summa = (a,b) => a+b;
+const kerto = (a,b) => a*b;
+const jako = (a,b) => a/b;
+const vahennys = (a,b) => a-b;
+module.exports = {summa, kerto, jako, vahennys};
